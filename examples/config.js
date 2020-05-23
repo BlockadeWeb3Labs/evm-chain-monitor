9a8a8623c8891e7c9c5194a2774cc138df482b3d
Cybourgeoisie <rbenheidorn@gmail.com>
2020-05-23 14:11:45 -0400

    Handle non-infura endpoints

--- examples/config.js
+++ examples/config.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
 
+let rinkeby_endpoint, mainnet_endpoint;
+if (process.env.RINKEBY_ENDPOINT) {
+	rinkeby_endpoint = process.env.RINKEBY_ENDPOINT;
+} else {
+	rinkeby_endpoint = "https://rinkeby.infura.io/v3/" + process.env.INFURA_KEY;
+}
+
+if (process.env.MAINNET_ENDPOINT) {
+	mainnet_endpoint = process.env.MAINNET_ENDPOINT;
+} else {
+	mainnet_endpoint = "https://mainnet.infura.io/v3/" + process.env.INFURA_KEY;
+}
+
 module.exports = {
 	chain : {
 		ethereum_rinkeby : {
 			'type'      : "Ethereum",
 			'name'      : "Ethereum Rinkeby",
-			'endpoint'  : "https://rinkeby.infura.io/v3/" + process.env.INFURA_KEY,
+			'endpoint'  : rinkeby_endpoint,
 			'read_url'  : "",
 			'write_url' : "",
 			'chain_id'  : "*"
@@ -13,7 +26,7 @@ module.exports = {
 		ethereum_mainnet : {
 			'type'      : "Ethereum",
 			'name'      : "Ethereum",
-			'endpoint'  : "https://mainnet.infura.io/v3/" + process.env.INFURA_KEY,
+			'endpoint'  : mainnet_endpoint,
 			'read_url'  : "",
 			'write_url' : "",
 			'chain_id'  : "*"
